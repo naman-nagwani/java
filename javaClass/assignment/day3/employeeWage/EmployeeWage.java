6391c90cdcb7e229d7db4f9440f343e2d51a3f69
“naman-nagwani” <namannagwani@gmail.com>
2022-08-05 21:04:08 +0530

    UC3 completed

--- javaClass/assignment/day3/employeeWage/EmployeeWage.java
+++ javaClass/assignment/day3/employeeWage/EmployeeWage.java
@@ -1,21 +1,25 @@
 package javaClass.assignment.day3.employeeWage;
-
 public class EmployeeWage {
 
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOUR = 8;
 
+    private static final int HALF_DAY_HOUR = 4;
+
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage computation");
-        int empPresent = (int) Math.floor(Math.random() * 10) % 2;
+        int empPresent = (int) Math.floor(Math.random() * 10) % 3;  // 0 and 1 // 0.87 // 8.7
         System.out.println(empPresent);
+        int dailyEmployeeWage = 0;
         if (empPresent == 1) {
-            System.out.println("Employee Present");
-            int dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Daily Employee Wage:"+dailyEmployeeWage);
-
+            System.out.println("Employee Present Full day");
+            dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else if (empPresent == 2) {
+            System.out.println("Employee Present Half day");
+            dailyEmployeeWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
         } else {
             System.out.println("Employee Absent");
         }
+        System.out.println("Daily Employee Wage:" + dailyEmployeeWage);
     }
 }
