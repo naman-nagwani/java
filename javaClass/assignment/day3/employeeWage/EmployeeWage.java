5e7ea4eca54ddf53af9b5ae31943ddfe35bc7b00
“naman-nagwani” <namannagwani@gmail.com>
2022-08-05 20:22:22 +0530

    Employee wage start

---
+++ javaClass/assignment/day3/employeeWage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package javaClass.assignment.day3.employeeWage;
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage computation");
+        }
+}
