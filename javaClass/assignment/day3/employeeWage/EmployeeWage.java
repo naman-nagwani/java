21092996836328d7a983dc1b2e73266c5b891ef2
“naman-nagwani” <namannagwani@gmail.com>
2022-08-05 21:12:25 +0530

    UC6 completed

--- javaClass/assignment/day3/employeeWage/EmployeeWage.java
+++ javaClass/assignment/day3/employeeWage/EmployeeWage.java
@@ -2,24 +2,47 @@ package javaClass.assignment.day3.employeeWage;
 public class EmployeeWage {
 
     private static final int WAGE_PER_HOUR = 20;
-    private static final int FULL_DAY_HOUR = 8;
+    private static final int FULL_DAY_HOUR = 10;
 
-    private static final int HALF_DAY_HOUR = 4;
+    private static final int HALF_DAY_HOUR = 5;
+
+    private static final int IS_PART_TIME = 1;
+
+    private static final int IS_FULL_TIME = 2;
+
+    private static final int WORKING_DAYS_PER_MONTH = 20;
+
+    private static final int TOTAL_WORKING_HOURS = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage computation");
-        int empPresent = (int) Math.floor(Math.random() * 10) % 3;  // 0 and 1 // 0.87 // 8.7
-        System.out.println(empPresent);
         int dailyEmployeeWage = 0;
-        if (empPresent == 1) {
-            System.out.println("Employee Present Full day");
-            dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        } else if (empPresent == 2) {
-            System.out.println("Employee Present Half day");
-            dailyEmployeeWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
-        } else {
-            System.out.println("Employee Absent");
+        int daysCount = 0;
+        int totalWagesPerMonth = 0;
+        int totalHours = 0;
+        while (daysCount < WORKING_DAYS_PER_MONTH  &&  totalHours <= TOTAL_WORKING_HOURS) {
+            int empPresent = (int) (Math.random() * 10) % 3;  // between 0 and 1 // 0.87 // 8.7 // 8.0 // 3.0
+            System.out.println("Total Hours:"+totalHours);
+            switch (empPresent) {
+                case IS_FULL_TIME:
+                    dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    totalHours += FULL_DAY_HOUR;
+                    break;
+                case IS_PART_TIME:
+                    dailyEmployeeWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                    totalHours += HALF_DAY_HOUR;
+                    break;
+                default:
+                    dailyEmployeeWage = 0;
+            }
+            System.out.println("Employee wage for Day #" + (daysCount+1) + " is " + dailyEmployeeWage);
+
+            totalWagesPerMonth += dailyEmployeeWage;
+            daysCount++;
         }
-        System.out.println("Daily Employee Wage:" + dailyEmployeeWage);
+
+        System.out.println("Total Employee Wage for a month:" + totalWagesPerMonth);
     }
+
+
 }
