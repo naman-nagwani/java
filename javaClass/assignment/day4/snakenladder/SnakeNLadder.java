5c26ceb1485d2dbd11490e597f5c154714598f81
“naman-nagwani” <namannagwani@gmail.com>
2022-08-05 21:32:47 +0530

    snake n ladder

---
+++ javaClass/assignment/day4/snakenladder/SnakeNLadder.java
@@ -0,0 +1,9 @@
+package javaClass.assignment.day4.snakenladder;
+
+public class SnakeNLadder {
+    static int POSITION=0; //current position
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And ladder");
+        System.out.println("Current Position : " + POSITION);
+    }
+}
