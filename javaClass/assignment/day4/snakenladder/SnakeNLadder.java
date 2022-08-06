a79ada599a59396e885906ebce08436bdbd7584b
“naman-nagwani” <namannagwani@gmail.com>
2022-08-06 05:42:21 +0530

    UC2 Completed

--- javaClass/assignment/day4/snakenladder/SnakeNLadder.java
+++ javaClass/assignment/day4/snakenladder/SnakeNLadder.java
@@ -1,9 +1,21 @@
 package javaClass.assignment.day4.snakenladder;
 
+import java.util.Random;
+
 public class SnakeNLadder {
     static int POSITION=0; //current position
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And ladder");
         System.out.println("Current Position : " + POSITION);
+        int dieNumber = dieRoll();
+        System.out.println("Die Number is : " +dieNumber);
+
+    }
+
+    private static int dieRoll() {
+        Random random = new Random();
+        int dieNumber = random.nextInt(6)+1;
+        return dieNumber;
+
     }
 }
